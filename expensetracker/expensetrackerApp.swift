fba399c69b5f605f4df5db3705a8d168493a7989
Justin Meeks <jtmeeks29050@gmail.com>
2025-02-15 20:51:57 -0500

    Initial Commit

---
+++ expensetracker/expensetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensetrackerApp.swift
+//  expensetracker
+//
+//  Created by Justin Meeks on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct expensetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expensetrackerTests/expensetrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  expensetrackerTests.swift
+//  expensetrackerTests
+//
+//  Created by Justin Meeks on 2/15/25.
+//
+
+import Testing
+@testable import expensetracker
+
+struct expensetrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
